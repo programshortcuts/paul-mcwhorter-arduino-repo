9f82b555436a4b03dff654dcea6b3ee12147bc54
tt <tt@MacBookPro.lan>
2026-07-01 09:24:34 -0400

    main

---
+++ playlist/js/nav/keyboard-nav.js
@@ -0,0 +1,21 @@
+// keyboard.js
+import { getFocusZone } from "./focus-zones.js"
+import { sideBarNav } from "./side-bar-nav.js"
+const navState = {
+    zone: null,
+    isLetterNav: false
+}
+export function initKeyboardNav({e,container}){
+    navState.zone = getFocusZone(e)
+    // should i query side-bar a elements here?
+    routeKey(e)   
+}
+function routeKey(e){
+    // object destructuring
+    const {zone,isLetterNav} = navState
+    // should i query side-bar a elements here?
+    if(zone === 'side-bar'){
+        const isHandled = sideBarNav(e)
+        if(isHandled) return
+    }
+}
