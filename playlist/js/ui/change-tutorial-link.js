b65f5deb766f89e86baacb51f2001a24ff1d223f
tt <tt@MacBookPro.lan>
2026-07-03 04:32:44 -0400

    main

--- playlist/js/ui/change-tutorial-link.js
+++ playlist/js/ui/change-tutorial-link.js
@@ -1,6 +1,12 @@
 export const tutorialLink = document.querySelector('#tutorialLink')
 export function changeTutorialLink({e}){
     if(!e.target.dataset.timestamp) return
-    let newHref = tutorialLink.href + "&t=" + e.target.dataset.timestamp
-    tutorialLink.href = newHref
+    const vidEL = e.target.closest('[data-video]')
+    const vidBase = vidEL.dataset.video
+    const ts = vidEL.dataset.timestamp
+
+    tutorialLink.dataset.timestamp = ts || ''
+
+    tutorialLink.href = ts ? `${vidBase}&t=${ts}s` :vidBase
+    console.log(tutorialLink.href)
 }
