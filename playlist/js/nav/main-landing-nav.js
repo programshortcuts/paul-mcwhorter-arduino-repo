53926c597e4cfed1f95dcc9d986417bb5fdd8522
tt <tt@MacBookPro.lan>
2026-07-03 02:09:51 -0400

    main

--- playlist/js/nav/main-landing-nav.js
+++ playlist/js/nav/main-landing-nav.js
@@ -14,25 +14,23 @@ export function refreshSteps() {
 }
 export function mainLandingNav(e) {
     const key = e.key.toLowerCase();
-
     // always safe if DOM changed
     if (!steps.length) updateSteps();
-
     if (!steps.length) return false;
-
+    if(!isNaN(key)){
+        const intKey = parseInt(key)
+        steps[intKey -1].focus()
+    }
     if (key === "f") {
         iStep = (iStep + 1) % steps.length;
         steps[iStep]?.focus();
         return true;
     }
-
     if (key === "a") {
         iStep = (iStep - 1 + steps.length) % steps.length;
         steps[iStep]?.focus();
         return true;
     }
-
-
     if (key == 's') {
         if (e.target != sideBarBtn) {
             sideBarBtn.focus()
